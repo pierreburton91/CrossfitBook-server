7a7babb6c4a15d5ace23cefb376d033eb44cc4d1
Pierre Burton <Pierre.Burton@alliance-bokiau.com>
2019-04-15 12:46:20 +0200

    remove unnecessary code

--- index.js
+++ index.js
@@ -12,31 +12,31 @@ const app = express();
 const configDB = require('./config/database.js');
 mongoose.connect(configDB.url, { useMongoClient: true });
 
-require('./config/passport')(passport);
+// require('./config/passport')(passport);
 
 app.use(compression());
 app.use(helmet())
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session({
-	store: new MemoryStore({
-		checkPeriod: 86400000 // Removes expired entries every 24h
-	}),
-	secret: 'crossfitBookisthebestapp',
-	resave: true,
-	saveUninitialized: false 
-}));
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
+// app.use(session({
+// 	store: new MemoryStore({
+// 		checkPeriod: 86400000 // Removes expired entries every 24h
+// 	}),
+// 	secret: 'crossfitBookisthebestapp',
+// 	resave: true,
+// 	saveUninitialized: false 
+// }));
+// app.use(passport.initialize());
+// app.use(passport.session()); // persistent login sessions
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
   });
 
-require('./routes/routes.js')(app, passport, request, upload, imgurID);
+require('./routes/routes.js')(app);
 
 app.listen(process.env.PORT || 3000, function() {
-	console.log('App is up and running!');
-})
+	console.log('App is up and running!!');
+});
