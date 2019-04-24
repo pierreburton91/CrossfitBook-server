b05dbc1d421ad7564dd499b32ea8beb2d18fd47f
Pierre Burton <Pierre.Burton@alliance-bokiau.com>
2019-04-24 11:57:48 +0200

    add routes

---
+++ routes/routes.js
@@ -0,0 +1,30 @@
+module.exports = function (app) {
+    const auth = require('../controllers/auth-controller.js');
+    const records = require('../controllers/records-controller.js');
+    const benchmarks = require('../controllers/benchmarks-controller.js');
+
+    app.route('/auth')
+        .get(auth.connect)
+        .put(auth.update)
+        .delete(auth.delete);
+
+    app.route('/records')
+        .get(records.getAll)
+        .post(records.addNew)
+        .delete(records.delete);
+
+    app.route('/records/:recordTitle/:recordText')
+        .post(records.addNewDeep)
+        .put(records.updateDeep)
+        .delete(records.deleteDeep);
+
+    app.route('/benchmarks')
+        .get(benchmarks.getAll)
+        .post(benchmarks.addNew)
+        .delete(benchmarks.delete);
+
+    app.route('/benchmarks/:benchmarkTitle')
+        .post(benchmarks.addNewDeep)
+        .put(benchmarks.updateDeep)
+        .delete(benchmarks.deleteDeep);
+};
